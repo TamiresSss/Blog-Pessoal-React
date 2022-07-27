f2f2666f88b1f585f704828265eaa41fc06960ef
Tamires <tamiressantos9619@gmail.com>
2022-07-27 14:04:10 -0300

    Adicionando home txt e css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+function Home (){
+    return (
+        <>
+            <h1 className="titulo"> Home</h1>
+            <img src="https://s.aficionados.com.br/imagens/o-clube-da-luluzinha.jpg" alt="Imagem Tela inicial" className="img" ></img>
+        </>
+    );
+}
+export default Home;
